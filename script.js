1da6786496fc230b0a96ae5a22f36f8e4e409dba
allisunnn <alinnkka11@gmail.com>
2026-01-06 23:33:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const loadingText = document.getElementById("loading");
 
 console.log("JS działa");
 
-fetch("https://restcountries.com/v3.1/all?fields=name,capital,population,flags")
+// pobieranie danych z zewnętrznego API
+fetch("https://jsonplaceholder.typicode.com/users")
     .then(response => {
         console.log("Status HTTP:", response.status);
-
         if (!response.ok) {
             throw new Error("Błąd HTTP: " + response.status);
         }
@@ -17,26 +17,21 @@ fetch("https://restcountries.com/v3.1/all?fields=name,capital,population,flags")
 
         loadingText.remove();
 
-        data.slice(0, 12).forEach(country => {
-            const name = country.name.common;
-            const capital = country.capital ? country.capital[0] : "brak danych";
-            const population = country.population.toLocaleString();
-            const flag = country.flags.png;
-
+        data.forEach(user => {
             const card = document.createElement("div");
             card.className = "country-card";
 
             card.innerHTML = `
-                <img src="${flag}" alt="Flaga ${name}">
-                <h2>${name}</h2>
-                <p><strong>Stolica:</strong> ${capital}</p>
-                <p><strong>Populacja:</strong> ${population}</p>
+                <h2>${user.name}</h2>
+                <p><strong>Email:</strong> ${user.email}</p>
+                <p><strong>Miasto:</strong> ${user.address.city}</p>
+                <p><strong>Firma:</strong> ${user.company.name}</p>
             `;
 
             container.appendChild(card);
         });
     })
     .catch(error => {
-        console.error(error);
+        console.error("Błąd:", error);
         loadingText.textContent = "Błąd podczas pobierania danych.";
     });
